b10b2b1e976c48dc8493a581415aeb8f0dd9ad82
miguelmontcerv <63685649+miguelmontcerv@users.noreply.github.com>
2021-08-31 20:02:12 -0500

    fondos

--- Codigos_Por_Clase/Sesion4/ventas.py
+++
@@ -1,9 +0,0 @@
-ventas = [{"id": 1, "price": 23, "ship": 14, "margin": 10}, 
-          {"id": 1, "price": 230, "ship": 14, "margin": 5},
-          {"id": 1, "price": 130, "ship": 14, "margin": 2}]
-
-
-#Total de ventas => 383
-#Ganancia => 16.4
-
-print(ventas[1].price)
